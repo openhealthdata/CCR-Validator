325a220933b31a814ea800ce94dbd7c9ff2a8c22
Diego López León <dieguitoll@gmail.com>
2011-02-10 09:50:38 -0300

    add test for validator (ignored by the moment)

--- src/test/java/org/openhealthdata/validator/ValidatorManagerTest.java
+++ src/test/java/org/openhealthdata/validator/ValidatorManagerTest.java
@@ -1,9 +1,10 @@
 package org.openhealthdata.validator;
 
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,10 +17,45 @@ public class ValidatorManagerTest {
 
     @Test
     @Ignore
-    public void validateTest(){
-        File xml = new File("/ccr_sample.xml");
+    public void validateTest() throws IOException{
         ValidationManager validationManager = new ValidationManager();
-        String expectedResult = validationManager.validateToString(xml);
-        System.out.println(expectedResult);
+        String result = validationManager.validateToString(fileFromInputStream(getClass().getResourceAsStream("/ccr_sample.xml")));
+        String expected = expectedResultAsString(getClass().getResourceAsStream("/expectedResult.xml"));
+        Assert.assertEquals(expected, result);
+    }
+
+    private String expectedResultAsString(InputStream expectedStream) throws IOException {
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(
+                    new InputStreamReader(expectedStream, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } finally {
+            expectedStream.close();
+        }
+
+        String expectedContent = writer.toString();
+        if ("\r\n".equals(System.getProperty("line.separator"))) {
+          expectedContent = expectedContent.replaceAll("\r[^\n]", System.getProperty("line.separator"));
+        } else {
+          expectedContent = expectedContent.replaceAll("\r\n", System.getProperty("line.separator"));
+        }
+        return expectedContent;
+    }
+
+    private File fileFromInputStream(InputStream is) throws IOException{
+        File f = File.createTempFile("tempInput", ".ccr");
+        OutputStream out = new FileOutputStream(f);
+        byte buf[] = new byte[1024];
+        int len;
+        while ((len = is.read(buf)) > 0)
+            out.write(buf, 0, len);
+        out.close();
+        is.close();
+        return f;
     }
 }
