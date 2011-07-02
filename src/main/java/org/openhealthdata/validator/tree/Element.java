a3b63f90bc3f7ac3b140d468ca90037dd724f416
Wolfgang Laun <wolfgang.laun@gmail.com>
2011-07-02 18:05:04 +0200

    Removed all 'core' rules. Insertion of all XML elements is now done by recursively walking the DOM tree.
    Workaround for the JBRULES-3114 ($x: x not in ...)
    
    pom.xml: 5.2.0 no SNAPSHOT

---
+++ src/main/java/org/openhealthdata/validator/tree/Element.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2011 OpenHealthData, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openhealthdata.validator.tree;
+
+/**
+ * Represents a wrapper for an XML element inserted as a fact.
+ * @author wlaun
+ */
+public class Element {
+    private Xpath  xpath;
+    private Object object;
+
+    /**
+     * Constructor.
+     * @param xpath the Xpath object denoting the XML element
+     * @param object the fact object
+     */
+    public Element( Xpath xpath, Object object ){
+        this.xpath  = xpath;
+        this.object = object;
+    }
+    
+    /**
+     * Returns the Xpath denoting the XML element
+     * @return an Xpath object
+     */
+    public Xpath getXpath() {
+        return xpath;
+    }
+
+    /**
+     * Returns the fact object resulting from the XML element.
+     * @return an Object
+     */
+    public Object getObject() {
+        return object;
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString(){
+        return xpath.toString() + " => " + object.getClass().getSimpleName(); 
+    }
+}
